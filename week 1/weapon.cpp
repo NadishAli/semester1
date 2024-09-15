1d7fa812063b71b3f32ac2656fecec36ccfdc75f
NadishAli <nadishali523@gmail.com>
2024-09-15 10:34:54 +0500

    Tasks

---
+++ week 1/weapon.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main(){
+	system("color 0a");
+cout<<"   +--^----------,--------,-----,--------^,    "<<endl;
+cout<<"    | ||||||||    '--------'     |         O   "<<endl;
+cout<<"    '+---------------------------^----------|  "<<endl;
+cout<<"      ',---------,---------,--------------'    "<<endl;
+cout<<"        / XXXXXX /'|      /'                   "<<endl;
+cout<<"       / XXXXXX /  |      /'                   "<<endl;
+cout<<"      / XXXXXX /'--------'                     "<<endl;
+cout<<"     / XXXXXX /                                "<<endl;
+cout<<"    / XXXXXX /                                 "<<endl;
+cout<<"   (--------(                                  "<<endl;
+cout<<"    '------'                                   ";
+}
